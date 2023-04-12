2c53c164fb59ba60d83e7e9fa03f9a74337b48c9
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2023-04-12 19:56:46 +0100

    add run_test

---
+++ run_test.py
@@ -0,0 +1,29 @@
+from CardPole import CardPole
+from utils import NestablePool
+import multiprocessing as mp
+import numpy as np
+
+
+
+def worker(number):
+    algorithm = CardPole(show_game=False, is_classical=False, run_number=number)
+    algorithm.train()
+
+
+if __name__ == '__main__':
+
+    n_runs = 10
+
+    # Create a pool of workers
+    pool = NestablePool(mp.cpu_count())
+   
+    # Run the workers
+    pool.map(worker, range(n_runs))
+
+    # Close the pool
+    pool.close()
+
+    # Wait for the processes to finish
+    pool.join()
+
+    print("Done")
