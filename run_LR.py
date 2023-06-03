589396a24fa45340023586da88e09806a2021551
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2023-06-03 17:35:12 +0100

    increase n_processes

--- run_LR.py
+++ run_LR.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     }
     gridsearch = GridSearch(searchgrid)
 
-    n_processes = 50
+    n_processes = os.cpu_count()
 
     print(f"[INFO] Starting grid search | TOTAL RUNS: {len(gridsearch)} | TOTAL PROCESSES: {n_processes}")
 
